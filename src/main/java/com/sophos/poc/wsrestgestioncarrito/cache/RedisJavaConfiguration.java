78c3d44fe7349b22a9cfd39f4f0f4d4d59ee11ad
jeisson.osoriod <jeisson.osoriod@SBBOGLAPPROJO7.sophos.col.com>
2019-03-31 17:39:51 -0500

    Configuracion conexion redis

--- src/main/java/com/sophos/poc/wsrestgestioncarrito/cache/RedisJavaConfiguration.java
+++ src/main/java/com/sophos/poc/wsrestgestioncarrito/cache/RedisJavaConfiguration.java
@@ -1,16 +1,12 @@
 package com.sophos.poc.wsrestgestioncarrito.cache;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-import com.sophos.poc.wsrestgestioncarrito.util.ConfigurationProperties;
 
 
 @Configuration
@@ -18,14 +14,10 @@ import com.sophos.poc.wsrestgestioncarrito.util.ConfigurationProperties;
 @EnableRedisRepositories(basePackages = "com.sophos.poc.wsrestgestioncarrito.cache.repository")
 @PropertySource("classpath:application.properties")
 public class RedisJavaConfiguration {
-	@Autowired
-	private ConfigurationProperties cts;
+
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
-		int 	redisPort = Integer.parseInt(System.getenv().get(cts.POC_REDIS_PORT) );
-	    String  redisHost = System.getenv().get(cts.POC_REDIS_HOST) ;	    
-		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
-	    return new JedisConnectionFactory(redisStandaloneConfiguration);	    
+	    return new JedisConnectionFactory();	    
 	}
 	 
 	@Bean
