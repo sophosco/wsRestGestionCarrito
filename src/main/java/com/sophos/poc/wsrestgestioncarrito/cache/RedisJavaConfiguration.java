4c8f4be38af82c91cc394a0137691136e7054ef8
jeisson.osoriod <jeisson.osoriod@SBBOGLAPPROJO7.sophos.col.com>
2019-03-31 20:12:39 -0500

    actualizacion conexion redis

--- src/main/java/com/sophos/poc/wsrestgestioncarrito/cache/RedisJavaConfiguration.java
+++ src/main/java/com/sophos/poc/wsrestgestioncarrito/cache/RedisJavaConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -17,7 +18,8 @@ public class RedisJavaConfiguration {
 
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
-	    return new JedisConnectionFactory();	    
+	    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("redis-master", 6379);
+	    return new JedisConnectionFactory(redisStandaloneConfiguration);
 	}
 	 
 	@Bean
