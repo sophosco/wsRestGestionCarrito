8b0d1b6a90d92beb3f723b3777ccfcee0be0a8bb
Jeisson <jeisson.osoriod@sophossolutions.com>
2019-04-01 13:02:39 -0500

    Se actualiza confirguracion de redis para utilizar variables de entorno AB#1276 AB#1277

--- src/main/java/com/sophos/poc/wsrestgestioncarrito/cache/RedisJavaConfiguration.java
+++ src/main/java/com/sophos/poc/wsrestgestioncarrito/cache/RedisJavaConfiguration.java
@@ -2,6 +2,7 @@ package com.sophos.poc.wsrestgestioncarrito.cache;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,8 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+import com.sophos.poc.wsrestgestioncarrito.util.ConfigurationProperties;
+
 
 @Configuration
 @ComponentScan("com.sophos.poc.wsrestgestioncarrito.cache")
@@ -20,10 +23,12 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 public class RedisJavaConfiguration {
 
 	private static final Logger logger = LogManager.getLogger(RedisJavaConfiguration.class);
+	@Autowired
+	private ConfigurationProperties prp;
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
-	    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("redis-master", 6379);
-	    redisStandaloneConfiguration.setPassword(RedisPassword.of("Qr7XDpcMWp"));
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(prp.getRedisHost(), prp.getRedisPort());
+	    redisStandaloneConfiguration.setPassword(RedisPassword.of(prp.getRedisPass()));
 	    return new JedisConnectionFactory(redisStandaloneConfiguration);
 	}
 	 
@@ -31,7 +36,7 @@ public class RedisJavaConfiguration {
 	public RedisTemplate<String, Object> redisTemplate() {
 	    RedisTemplate<String, Object> template = new RedisTemplate<>();
 	    template.setConnectionFactory(jedisConnectionFactory());
-	    logger.info("redis-master:" +jedisConnectionFactory().getHostName());
+	    logger.info("Redis connection factory: " + prp.getRedisHost() + ":" +prp.getRedisPort());
 	    return template;
 	}
 
--- src/main/java/com/sophos/poc/wsrestgestioncarrito/util/ConfigurationProperties.java
+++ src/main/java/com/sophos/poc/wsrestgestioncarrito/util/ConfigurationProperties.java
@@ -13,9 +13,16 @@ public class ConfigurationProperties {
 	private static ConfigurationProperties instance = null;
 	private String cacheSource;
 	private String securityEndpointValidation;
-	
 	private static final String FILE = "configuration.properties";
 	private static final String CACHE_SOURCE = "cache.source";
+	private static final String REDIS_HOST = "redis.default.host";
+	private static final String REDIS_PORT = "redis.default.port";
+	private static final String REDIS_PSS = "redis.default.pass";
+	
+	private String redisHost;
+	private int	   redisPort;
+	private String redisPass;
+	
 	public final String APP_NAME = "wsRestGestionCarrito";
 	public final String SECURITY_SUCCESS = "00";
 	public final String CACHE_SOURCE_REDIS = "REDIS";
@@ -28,7 +35,7 @@ public class ConfigurationProperties {
 			
 	public ConfigurationProperties getInstance() {
 		if (instance == null) {
-			return instance = new ConfigurationProperties();
+			return new ConfigurationProperties();
 		}
 		return instance;
 	}
@@ -38,8 +45,20 @@ public class ConfigurationProperties {
 		try {
 			InputStream inputStream = ConfigurationProperties.class.getClassLoader().getResourceAsStream(FILE); 
 			pr.load(inputStream);
+			
+			String envRP = System.getenv().get("POC_REDIS_PORT");
+			String envRH = System.getenv().get("POC_REDIS_HOST");
+			String envRC = System.getenv().get("POC_REDIS_PASS");
+			
 			setCacheSource(pr.getProperty(CACHE_SOURCE));	
 			setSecurityEndpointValidation(System.getenv("POC_SERVICE_SECURITY_VALIDATE"));
+			setRedisHost(envRH != null && !envRH.equals("") ? envRH : pr.getProperty(REDIS_HOST));
+			setRedisPass(envRC != null && !envRC.equals("") ? envRC : pr.getProperty(REDIS_PSS));
+			setRedisPort(envRP != null && !envRP.equals("") ? Integer.parseInt(envRP) : Integer.parseInt(pr.getProperty(REDIS_PORT)) );
+			
+			
+		}catch (NumberFormatException ex) {
+			setRedisPort(6379);
 		}catch(Exception e) {
 			logger.error("ConfigurationProperties-Error inicializando las variables", e);
 		}
@@ -59,5 +78,29 @@ public class ConfigurationProperties {
 
 	public void setSecurityEndpointValidation(String securityEndpointValidation) {
 		this.securityEndpointValidation = securityEndpointValidation;
+	}
+
+	public String getRedisHost() {
+		return redisHost;
+	}
+
+	public void setRedisHost(String redisHost) {
+		this.redisHost = redisHost;
+	}
+
+	public int getRedisPort() {
+		return redisPort;
+	}
+
+	public void setRedisPort(int redisPort) {
+		this.redisPort = redisPort;
+	}
+
+	public String getRedisPass() {
+		return redisPass;
+	}
+
+	public void setRedisPass(String redisPass) {
+		this.redisPass = redisPass;
 	}
 }
