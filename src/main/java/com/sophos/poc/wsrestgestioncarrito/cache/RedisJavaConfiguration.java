de389883647aae571006ed1934b17c1220170cf1
jeisson.osoriod <jeisson.osoriod@SBBOGLAPPROJO7.sophos.col.com>
2019-03-31 20:41:13 -0500

    Atualizacion conexion redis

--- src/main/java/com/sophos/poc/wsrestgestioncarrito/cache/RedisJavaConfiguration.java
+++ src/main/java/com/sophos/poc/wsrestgestioncarrito/cache/RedisJavaConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -21,7 +22,8 @@ public class RedisJavaConfiguration {
 	private static final Logger logger = LogManager.getLogger(RedisJavaConfiguration.class);
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
-	    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("172.20.88.24", 6379);
+	    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("redis-master", 6379);
+	    redisStandaloneConfiguration.setPassword(RedisPassword.of("Qr7XDpcMWp"));
 	    return new JedisConnectionFactory(redisStandaloneConfiguration);
 	}
 	 
